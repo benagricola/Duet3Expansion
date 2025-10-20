709cd6bd1c39f77a298b26239a29b943670b289b
Ben Agricola <ben+git@agrico.la>
2025-10-20 22:13:39 +0100

    Fix RP2350 build on Linux
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- makefiles/Core.mk
+++ makefiles/Core.mk
@@ -6,8 +6,9 @@ CORE_SRC_DIRS  = src src/RP2040 src/RP2040/include/$(MCU_DIR)/pico_base src/RP20
 
 CORE_SRC = $(CORE_DIR) $(addprefix $(CORE_DIR)/, $(CORE_SRC_DIRS))
 CORE_INCLUDES = $(addprefix -I, $(CORE_SRC))
+CORE_INCLUDES += -I$(CORE_DIR)/src/arm/CMSIS/5.4.0/CMSIS/Core/Include
 SDK_INCLUDE_DIRS = boards/include common/pico_base_headers/include common/pico_sync/include common/pico_time/include rp2_common/cmsis/include rp2_common/hardware_base/include
-SDK_INCLUDE_DIRS += rp2_common/hardware_dma/Include rp2_common/hardware_gpio/include rp2_common/hardware_irq/include rp2_common/hardware_sync/include rp2_common/hardware_timer/include
+SDK_INCLUDE_DIRS += rp2_common/hardware_dma/include rp2_common/hardware_gpio/include rp2_common/hardware_irq/include rp2_common/hardware_sync/include rp2_common/hardware_timer/include
 SDK_INCLUDE_DIRS += rp2_common/hardware_watchdog/include $(MCU_DIR)/pico_platform/include $(MCU_DIR)/hardware_regs/include $(MCU_DIR)/hardware_structs/include
 SDK_INCLUDE_DIRS += rp2_common/cmsis/stub/CMSIS/Core/include rp2_common/cmsis/stub/CMSIS/Device/RP2040/Include rp2_common/hardware_adc/include rp2_common/pico_platform_compiler/include
 SDK_INCLUDE_DIRS += rp2_common/pico_unique_id/include rp2_common/hardware_pwm/include rp2_common/pico_multicore/include rp2_common/hardware_pio/include rp2_common/pico_platform_sections/include
@@ -15,6 +16,9 @@ SDK_INCLUDE_DIRS += rp2_common/hardware_spi/include rp2_common/hardware_i2c/incl
 SDK_INCLUDE_DIRS += rp2_common/hardware_sync_spin_lock/include rp2_common/pico_runtime_init/include rp2_common/pico_runtime/include common/pico_stdlib_headers/include
 SDK_INCLUDE_DIRS += rp2_common/pico_stdio/include rp2_common/hardware_uart/include rp2_common/hardware_resets/include rp2_common/hardware_clocks/include rp2_common/hardware_ticks/include
 SDK_INCLUDE_DIRS += rp2_common/pico_platform_common/include
+ifeq ($(PROCESSOR),RP2350)
+SDK_INCLUDE_DIRS += rp2_common/cmsis/include
+endif
 SDK_INCLUDE_PATHS = $(addprefix $(SDK_DIR)/src/, $(SDK_INCLUDE_DIRS))
 SDK_INCLUDE_PATHS += $(addprefix $(SDK_DIR)/, lib/tinyusb/src)
 SDK_INCLUDES = $(addprefix -I, $(SDK_INCLUDE_PATHS))
