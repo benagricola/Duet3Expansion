e2c86d224ca830fde6cc5bd5aec3bf3368e01518
Ben Agricola <ben+git@agrico.la>
2025-11-05 15:43:24 +0000

    Enable pulling driver temps from TMC2240 on expansion boards
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- src/Heating/Sensors/TmcDriverTemperatureSensor.cpp
+++ src/Heating/Sensors/TmcDriverTemperatureSensor.cpp
@@ -7,8 +7,17 @@
 
 #include "TmcDriverTemperatureSensor.h"
 #include <Movement/Move.h>
+#include <CanMessageGenericParser.h>
 
 #if HAS_SMART_DRIVERS
+
+#if SUPPORT_TMC2240
+# include <TMC22xx.h>
+#endif
+
+#if SUPPORT_TMC51xx
+# include <TMC51xx.h>
+#endif
 
 // Sensor type descriptors
 TemperatureSensor::SensorTypeDescriptor TmcDriverTemperatureSensor::typeDescriptor(TypeName, [](unsigned int sensorNum) noexcept -> TemperatureSensor *_ecv_from { return new TmcDriverTemperatureSensor(sensorNum); } );
@@ -23,6 +32,59 @@ void TmcDriverTemperatureSensor::Poll()
 	SetResult(moveInstance->GetTmcDriversTemperature(), TemperatureError::ok);
 }
 
-#endif
+#if SUPPORT_TMC2240 || (SUPPORT_TMC51xx && TMC_TYPE == 2240)
+
+// TmcDriverActualTemperatureSensor implementation
+TemperatureSensor::SensorTypeDescriptor TmcDriverActualTemperatureSensor::typeDescriptor(TypeName, [](unsigned int sensorNum) noexcept -> TemperatureSensor *_ecv_from { return new TmcDriverActualTemperatureSensor(sensorNum); } );
+
+TmcDriverActualTemperatureSensor::TmcDriverActualTemperatureSensor(unsigned int sensorNum) noexcept
+	: TemperatureSensor(sensorNum, "TMC driver temperature"), driverNumber(0)
+{
+}
+
+TmcDriverActualTemperatureSensor::~TmcDriverActualTemperatureSensor() noexcept
+{
+}
+
+GCodeResult TmcDriverActualTemperatureSensor::Configure(const CanMessageGenericParser& parser, const StringRef& reply)
+{
+	bool seen = false;
+	
+	uint8_t driver;
+	if (parser.GetUintParam('P', driver))
+	{
+		if (driver >= NumDrivers)
+		{
+			reply.printf("Driver number must be 0-%u", NumDrivers - 1);
+			return GCodeResult::error;
+		}
+		driverNumber = driver;
+		seen = true;
+	}
+	
+	if (!seen)
+	{
+		CopyBasicDetails(reply);
+		reply.catf(", driver %u", driverNumber);
+	}
+	
+	return GCodeResult::ok;
+}
+
+void TmcDriverActualTemperatureSensor::Poll() noexcept
+{
+	if (driverNumber >= NumDrivers)
+	{
+		SetResult(TemperatureError::hardwareError);
+		return;
+	}
+	
+	const float temp = SmartDrivers::GetDriverTemperature(driverNumber);
+	SetResult(temp, TemperatureError::ok);
+}
+
+#endif // SUPPORT_TMC2240 || (SUPPORT_TMC51xx && TMC_TYPE == 2240)
+
+#endif // HAS_SMART_DRIVERS
 
 // End
--- src/Heating/Sensors/TmcDriverTemperatureSensor.h
+++ src/Heating/Sensors/TmcDriverTemperatureSensor.h
@@ -25,6 +25,25 @@ private:
 	static SensorTypeDescriptor typeDescriptor;
 };
 
+#if SUPPORT_TMC2240 || (SUPPORT_TMC51xx && TMC_TYPE == 2240)
+// This class represents a sensor that reads the actual temperature from a TMC2240 driver
+class TmcDriverActualTemperatureSensor : public TemperatureSensor
+{
+public:
+	TmcDriverActualTemperatureSensor(unsigned int sensorNum) noexcept;
+	~TmcDriverActualTemperatureSensor() noexcept;
+
+	static constexpr const char *TypeName = "drivertemp";
+
+	void Poll() noexcept override;
+	GCodeResult Configure(const CanMessageGenericParser& parser, const StringRef& reply) override;
+
+private:
+	static SensorTypeDescriptor typeDescriptor;
+	uint8_t driverNumber;
+};
+#endif
+
 #endif
 
 #endif /* SRC_HEATING_SENSORS_TMCDRIVERTEMPERATURESENSOR_H_ */
