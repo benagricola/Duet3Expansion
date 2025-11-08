2e8177f07815f60ea132537bae7f723e5c9eb203
Ben Agricola <ben+git@agrico.la>
2025-11-08 00:47:25 +0000

    Fix incorrect pin being passed to TmcDriverStates::Init()
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- src/Movement/StepperDrivers/TMC22xx.cpp
+++ src/Movement/StepperDrivers/TMC22xx.cpp
@@ -2433,7 +2433,7 @@ void SmartDrivers::Init() noexcept
 
 		driverStates[drive].Init(drive
 #if TMC22xx_HAS_ENABLE_PINS
-								, DriverEnablePins[drive]
+								, EnablePins[drive]
 #endif
 #if TMC22xx_VARIABLE_NUM_DRIVERS && TMC22xx_MULTIPLE_UART_PINS
 								, Tmc22xxUartPins[drive]
